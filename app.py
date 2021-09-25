4317620562f8161627d65efbaee98532c8ce19e0
pustaks <91389618+pustaks@users.noreply.github.com>
2021-09-25 18:51:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ import tempfile
 
 import autopy
 import img2pdf
+import ctypes
+import time
 
-
-def screenshot(top_left, right_bottom, next_page, total_page):
+def screenshot(top_left, right_bottom, main_window, total_page):
     rect_size = (right_bottom[0] - top_left[0], right_bottom[1] - top_left[1])
     images = []
     temp_dir = tempfile.mkdtemp()
+    
+    autopy.mouse.move(*main_window)
+    autopy.mouse.click(delay=0.1)
+    
     for i in range(total_page):
         page_num = "{}".format(i).zfill(len(str(total_page)))
         file_name = os.path.join(temp_dir, 'book-page-{}.png'.format(page_num))
         images.append(file_name)
 
-        autopy.mouse.move(*next_page)
-        autopy.mouse.click(delay=1)
+        time.sleep(1)
         autopy.bitmap.capture_screen((top_left, rect_size)).save(file_name)
+        ctypes.windll.user32.keybd_event(0x22,0,0,0)
 
     return images
 
